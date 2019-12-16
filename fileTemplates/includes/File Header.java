44ca033182c056549786412568919cf043f951ab
wanlei <roy.wan@upltv.com>
2019-12-16 20:47:01 +0800

    AI-3.5.2 <Avidly@DESKTOP-PD7PTJ7 Create baseRefactoring.xml, ide.general.xml, _@user_Default.icls, project.default.xml, Singleton.java, Enum.java, Default copy.xml, Interface.java, roy.xml, Default.xml, roygroup.xml, androidStudioFirstRun.xml, find.xml, ui.lnf.xml, File Header.java, filetypes.xml, projectView.xml, keymap.xml, code.style.schemes.xml, notifications.xml, AnnotationType.java, laf.xml, Class.java, jdk.table.xml, path.macros.xml, editor.codeinsight.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,9 @@
+/**     
+  * @ProjectName:    ${PROJECT_NAME}
+  * @Package:        ${PACKAGE_NAME}
+  * @ClassName:      ${NAME}
+  * @Description:     java类作用描述 
+  * @Author:         作者名
+  * @CreateDate:     ${DATE} ${TIME}
+ */
+
