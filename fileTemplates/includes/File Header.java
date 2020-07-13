9bcfb0167c138c918d601ad3db5bd1e61cba6c23
wawo00 <uruz3080@gmail.com>
2020-07-13 17:04:20 +0800

    AI-3.5.3 <wan@MacBook-Pro-3.local Overwrite remote https://github.com/wawo00/MyAndroidStudioSetting.git to local

--- fileTemplates/includes/File Header.java
+++
@@ -1,9 +0,0 @@
-/**     
-  * @ProjectName:    ${PROJECT_NAME}
-  * @Package:        ${PACKAGE_NAME}
-  * @ClassName:      ${NAME}
-  * @Description:     java类作用描述 
-  * @Author:         作者名
-  * @CreateDate:     ${DATE} ${TIME}
- */
-
